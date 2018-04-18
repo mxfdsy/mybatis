83a630e6194ef41cb0db8aec178c846693738d2e
平凡的世界 <963559210@qq.com>
2018-04-18 23:38:34 +0800

    主键查询不对

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 import dao.StudentMapper;
+import dao.TeacherMapper;
 import entity.Student;
+import entity.Teacher;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
@@ -14,16 +17,38 @@ public class Main {
         InputStream in = Main.class.getResourceAsStream("resource/mybatis-cfg.xml");
         SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(in);
         SqlSession session =factory.openSession();
-        StudentMapper mapper = session.getMapper(StudentMapper.class);
-
-        Student student = new Student();
+//        StudentMapper mapper = session.getMapper(StudentMapper.class);
+//        Student student = new Student();
 //        student.setId(3);
 //        student.setName("没想法的岁月");
 //        student.setNumber(1995);
 //        mapper.insertStudent(student);
 //        mapper.selectStudentId(1);
         //一对一查询
-         student = mapper.selectByPrimaryKeyWithTeacher(1);
+//         Student student1 = mapper.selectByPrimaryKeyWithTeacher(1);
+        //一对多查询
+//        TeacherMapper mapper = session.getMapper(TeacherMapper.class);
+//        Teacher teacher = new Teacher();
+//        teacher = mapper.selectByPrimaryKeyWithStudent(1);
+        //插入返回主键
+        StudentMapper studentMapper = session.getMapper(StudentMapper.class);
+
+        TeacherMapper teacherMapper = session.getMapper(TeacherMapper.class);
+
+//        Teacher  teacher = new Teacher();
+//        teacher.setTeacherName("我是老师");
+//        teacher.setPassword("cww123456");
+//        teacherMapper.insertSelective(teacher);
+//        Student student = new Student();
+//        student.setStudentName("我是学生");
+//        student.setStudentNumber(7788);
+//        student.setsId(teacher.gettId());
+//        studentMapper.insertSelective(student);
+        //传入一个集合查询参数
+        List<Integer> ids = new ArrayList<>();
+        ids.add(1);
+        ids.add(2);
+        List<Teacher> teachers = teacherMapper.selectByTids(ids);
         session.commit();
         session.close();
     }
